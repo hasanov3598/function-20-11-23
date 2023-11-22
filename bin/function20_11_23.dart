675fd39deecfb1badec32fc3ab6877e732301647
hasanov3598 <bahramov3598@gmail.com>
2023-11-22 19:59:20 +0600

    first commit

---
+++ bin/function20_11_23.dart
@@ -0,0 +1,68 @@
+import 'dart:io';
+
+//void main() {}
+//   print(func(-1));
+// }
+
+// bool func(int a) {
+//   if (a <= 0) {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// }
+
+
+//   bool a = true;
+//   trueOrFalse(a);
+// }
+
+// void trueOrFalse(bool isTrue) {
+//   if (isTrue) {
+//     print("Переменная имеет значение тру");
+//   } else {
+//     print("Переменная имеет значение фолс");
+//   }
+// }
+
+
+
+//   List array = [1, 2, 3, 4, 5, 6];
+//   print(func(array));
+// }
+//   print("Введите минуты");
+//   int min = int.parse(stdin.readLineSync()!);
+//   print("в  $min минут ${rerurnSec(min)} секунд");
+// }
+//   print(returnSqr(4));
+
+//   print(returnSumm(a: 10.2, b: 1.9));
+
+//   print(return10(5));
+// }
+
+// int returnSqr(int a) {
+//   return a * a;
+// }
+
+// double returnSumm({required double a, required double b}) {
+//   return a + b;
+// }
+
+// int return10(int a) {
+//   int summ = 1;
+//   for (int i = 0; i < 3; i++) {
+//     summ *= a;
+//   }
+
+//   return summ;
+
+// }
+
+// int rerurnSec(int min) {
+//   return min * 60;
+// }
+
+// int func(List a) {
+//   return a.first;
+// }
---
+++ test/function20_11_23_test.dart
@@ -0,0 +1,8 @@
+import 'package:function20_11_23/function20_11_23.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
